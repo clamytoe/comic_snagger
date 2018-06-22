aff55fa067bd2a772f9fee1ddf674660118480d5
Martin Uribe <clamytoe@users.noreply.github.com>
2018-06-22 13:56:31 -0500

    Backed up old script to reference while I completely rewrite the program to work with a whole new site.

--- comicSnagger.py
+++ comicSnagger.py
@@ -1,207 +1,117 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
-Scrape http://readcomics.website for comic book images
+Scrape https://www.readcomics.io for comic book images
 """
 import json
 import os
 import shutil
+from collections import defaultdict, namedtuple
 from sys import exit
 
 import requests
 from bs4 import BeautifulSoup
 
 
-def collect_image_data(chapters):
-    """
-    Scrapes the images for the comics
-    """
-    combined_total = 0
-    comicbooks = {}
-
-    # chapters contains lists consisting of [title, url]
-    for chapter in chapters:
-        title = chapter[0]
-        url = chapter[1]
-        # Scrape images in linked pages
-        comicbooks[title] = scrape_links(url)
-        # Display the name of the comic along with how many images it contains
-        page_count = len(comicbooks[title])
-        combined_total += page_count
-        print("  {0} has {1} pages".format(title, page_count))
-
-    print("Total images to be processed: {}".format(combined_total))
-
-    return comicbooks
+Comic = namedtuple("Comic", "title url")
+LOCAL_DIR = "/home/mohh/Downloads/Comics/"
+URL = "https://www.readcomics.io"
+SEARCH_URL = f"{URL}/comic-search?key="
 
 
-def exists(url):
-    """
-    Determines if the provided url exists
-    """
-    page = requests.head(url)
-    status = page.status_code
-    return True if status == requests.codes.ok else False
-
-
-def scrape_chapters(base_url, main_title):
-    """
-    Scrape the title and url of all the comics in the page
-    """
-    soup = None
-
-    # Try to retrieve the list of chapters
+def display_choice(search_term):
+    url = SEARCH_URL + search_term.replace(" ", "+")
+    comics = search(url)
+    print(f"Found {len(comics)} titles matching {search_term}")
+    for i, comic in enumerate(comics):
+        print(f" [{i}] {comic.title}")
     try:
-        print("Processing: {}...".format(main_title))
-        page = requests.get(base_url)
-        soup = BeautifulSoup(page.content, "html.parser")
-        page.close()
-    except requests.exceptions.ConnectionError:
-        print("You must be connected to the Internet in order for this to work...")
-        exit(1)
-
-    chapters = soup.find_all(class_="chapter-title-rtl")
-    comics = [[c.contents[1].next, c.contents[1]["href"]] for c in chapters]
-
-    return comics
+        choice = int(input(f"\nWhich one would you like to get? "))
+        get_comic(comics[choice])
+    except ValueError as e:
+        print(f"\n  {e}")
 
 
-def scrape_links(url):
-    """
-    Scrape links pointing to the comic book images
-    """
-    images = []
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    images.append(soup.find(class_="img-responsive scan-page")["src"].strip())
-
-    # to avoid multiple get requests, we're only going to check and see if the image exists
-    page_url, img_num = images[-1].rsplit("/", 1)
-    num, ext = img_num.split(".")
-    num = int(num)
-
-    # increase image number until the image is not found
-    while True:
-        num += 1
-        img_num = str(num).zfill(2)
-        image_url = page_url + "/{0}.{1}".format(img_num, ext)
-
-        # if image is found add it to the list otherwise break out of the loop
-        if exists(image_url):
-            images.append(image_url)
-        else:
-            break
-
-    return images
+def download(url):
+    soup = get_soup(url+"/full")
+    images = soup.find_all(class_="chapter_img")
+    links = [link["src"] for link in images]
+    for link in links:
+        print(f"Downloading: {link}")
 
 
-def download_images(local_dir, main_title, comicbooks):
-    """
-    Retrieve and save all of the images
-    """
-    for comic in comicbooks.keys():
-        title_dir = os.path.join(local_dir, main_title)
-        comic_dir = os.path.join(title_dir, comic)
-        print("\nProcessing: {}...".format(comic))
+def get_comic(comic):
+    print(f"Retrieving: {comic.title}")
+    print(comic.url)
+    chapters = []
+    soup = get_soup(comic.url)
 
-        # Create the main directory for our title
-        try:
-            os.makedirs(title_dir)
-        except FileExistsError:
-            pass
+    genres_ul = soup.find(class_="anime-genres")
+    genres = [g.text for g in genres_ul.find_all("a")]
+    print(f"{' '.join(genres)}")
 
-        if not os.path.isfile(comic_dir + ".cbz"):
-            for img in comicbooks[comic]:
-                image = img.split("/")[-1]
-                num, ext = image.split(".")
+    desc_div = soup.find(class_="detail-desc-content")
+    desc = desc_div.find("p").text.strip()
+    print(f"\n  {desc}\n")
 
-                # Pad the number with a zero
-                image = (
-                    "{0}.{1}".format(num.zfill(2), ext)
-                    if int(num) < 10 and len(num) == 1
-                    else image
-                )
-                filename = os.path.join(comic_dir, image)
-                try:
-                    os.makedirs(comic_dir)
-                except FileExistsError:
-                    pass
+    chapter_a = soup.find_all(class_="ch-name")
+    for link in chapter_a:
+        title = link.text
+        url = link["href"]
+        chapters.append(Comic(title, url))
 
-                cmd = 'wget --no-verbose --show-progress -c {0} -O "{1}"'.format(
-                    img, filename
-                )
-                os.system(cmd)
-
-            compress_comic(comic_dir)
-        else:
-            print('"{}" already exists, skipping'.format(comic_dir.split("/")[-1]))
-
-
-def compress_comic(comic_dir):
-    """
-    Takes the given directory and compresses it into the comic book format
-    """
-    if os.path.isdir(comic_dir):
-        print("Creating comic book file: {}.cbz".format(comic_dir.split("/")[-1]))
-        shutil.make_archive(comic_dir, "zip", comic_dir)
-        os.rename(comic_dir + ".zip", comic_dir + ".cbz")
-        shutil.rmtree(comic_dir)
+    count = len(chapters)
+    if count == 1:
+        descriptive = "is"
+        s = ""
     else:
-        print("\nInvalid directory path was given:\n\t {}".format(comic_dir))
-        exit(2)
+        descriptive = "are"
+        s = "s"
+    print(f"There {descriptive} {count} chapter{s} available:")
+
+    for i, chapter in enumerate(chapters):
+        print(f" [{i}] {chapter.title}")
+    choice = input("\nWhich one would you like? [ENTER] for all ")
+    if not choice:
+        for chapter in chapters:
+            download(chapter.url)
+    else:
+        try:
+            choice = int(choice)
+            download(chapters[choice].url)
+        except ValueError as e:
+            print(f"\n  {e}")
 
 
-def save(directory, title, dictionary):
-    """
-    Takes a directory path and a dictionary object and dumps it to a json file
-    """
-    comic_log = os.path.join(directory, title)
-    with open("{}.json".format(comic_log), "w") as f:
-        json.dump(dictionary, f)
+def get_soup(url):
+    page = requests.get(url)
+    if page.ok:
+        soup = BeautifulSoup(page.content, "html.parser")
+        return soup
+    print("Something's gone wrong, sorry...")
+    exit(1)
 
 
 def main():
-    """
-    Get the comic's main page and follow the links to get all of the chapters that are available
-    """
-    local_dir = "/home/mohh/Downloads/Comics/"
+    search_term = input("Comic name: ")
+    display_choice(search_term)
 
-    # Get main page and get links to all of the chapter pages
-    # base_url = 'http://readcomics.website/comic/dungeons-dragons-frost-giants-fury-2017'
-    # base_url = 'http://readcomics.website/comic/guidebook-to-the-marvel-cinematic-universe-marvels-doctor-strange'
-    # base_url = 'http://readcomics.website/comic/dollface-st-patricks-day-special-2017'
-    base_url = "http://readcomics.website/comic/my-little-pony-friendship-is-magic-2012"
 
-    # Create the main directory for our title
+def search(search_url):
+    comics = []
     try:
-        os.makedirs(local_dir)
-    except FileExistsError:
-        pass
-
-    # Extract the title from the url
-    main_title = ((base_url.split("/")[-1]).replace("-", " ")).title()
-
-    filename = "{}.json".format(main_title)
-    comic_cache = os.path.join(local_dir, filename)
-
-    if not os.path.isfile(comic_cache):
-        # Scrape chapters in the main page
-        chapters = scrape_chapters(base_url, main_title)
-
-        print("Found: {} comics".format(len(chapters)))
-        print("Collecting image data...")
-
-        comicbooks = collect_image_data(chapters)
-        save(local_dir, main_title, comicbooks)
-    else:
-        print("Found cached file: {}".format(comic_cache))
-        with open(comic_cache, "r") as f:
-            comicbooks = json.load(f)
-
-    # Download all of the images for each comic
-    download_images(local_dir, main_title, comicbooks)
-
-    print("\nAll comics have been retrieved and compiled.")
-
+        term = search_url.split('=')[1].replace('+', ' ').title()
+        print(f"Searching for: {term}...")
+        soup = get_soup(search_url)
+        series = soup.find_all(class_="egb-serie")
+        for link in series:
+            title = link.text
+            url = link["href"]
+            comics.append(Comic(title, url))
+        return comics
+    except requests.exceptions.ConnectionError:
+        print("You must have an active Internet connection to use...")
+        exit(1)
+    
 
 if __name__ == "__main__":
     main()
